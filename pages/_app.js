82bbd011d757caaa4b74d4bb1271cc8d12257acf
aulth <mohdusman.you@gmail.com>
2022-05-16 12:39:12 +0530

    added meta viewport

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,7 @@ import Search from '../components/Search'
 import {useState, useEffect, useContext} from 'react'
 import Cartstate from '../components/context/Cartstate'
 import {useRouter} from 'next/router'
+import Head from 'next/head'
 function MyApp({ Component, pageProps }) {
   const [userLoggedIn, setUserLoggedIn] = useState(false)
   let searchBarFree = ['/login', '/signup', '/forgot-password', '/product/[item]', '/about', '/services-products', '/contact']
@@ -21,6 +22,9 @@ function MyApp({ Component, pageProps }) {
   {
     searchBarFree.includes(router.pathname) ? null : <Search/>
   }
+  <Head>
+  <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0, maximum-scale=1.0"/>
+  </Head>
   <Component {...pageProps} />
   <Footer/>
   </Cartstate>
